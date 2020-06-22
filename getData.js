5269c0a54ea68e3958f9f5afce79e020e18c5e9f
caevee <pister.alex.cv@gmail.com>
2020-06-22 18:30:01 +0200

    added start script which updates data.json

---
+++ getData.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const fs = require('fs');
+
+const data = axios.get("https://raw.githubusercontent.com/LucaKaufmann/video-data/master/v2/data.json")
+    .then(function (response) {
+        console.log(response.data);
+        return response.data;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+data
+    .then((data) => {
+        fs.writeFileSync('data.json', JSON.stringify(data));
+    })
+    .catch(err => console.log(err)
+    );
